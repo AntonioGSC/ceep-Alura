9f47afe5a324faa62be21ca65f063d019e93be05
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-05-04 19:50:23 -0300

    handler

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import './formularioCadastro.css';
 
 export class FormularioCadastro extends Component {
+  constructor() {
+    super();
+    this.titulo = '';
+    this.corpo = '';
+  }
+
+  handleMudancaTitulo(event) {
+    this.titulo = event.target.value;
+  }
+
+  handleMudancaCorpo(event) {
+    this.corpo = event.target.value;
+  }
+
   render() {
     return (
       <form className="form-cadastro ">
@@ -9,11 +23,13 @@ export class FormularioCadastro extends Component {
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this.handleMudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
           placeholder="Escreva sua nota..."
           className="form-cadastro_input"
+          onChange={this.handleMudancaCorpo.bind(this)}
         />
         <button className="form-cadastro_input form-cadastro_submit">
           Criar Nota
