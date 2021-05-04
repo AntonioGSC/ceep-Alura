c87bead41607f2f0d8312ce5297de219b1390809
AntonioGSC <antoniogabrielagsc@gmail.com>
2021-05-04 19:17:58 -0300

    add css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import FormularioCadastro from './components/FormularioCadastro.jsx';
-import ListaDeNotas from './components/ListaDeNotas.jsx';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro.jsx';
+import ListaDeNotas from './components/ListaDeNotas/ListaDeNotas.jsx';
+import './assets/index.css';
+import './assets/App.css';
 
 export class App extends Component {
   render() {
     return (
-      <section>
+      <section className="conteudo">
         <FormularioCadastro />
         <ListaDeNotas />
       </section>
